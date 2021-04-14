5fa4732ab9ddd958f449ae23745087dc98556215
HafiszahSaidatul <hafiszahsaidatull@gmail.com>
2021-04-14 22:15:00 +0700

    Menyelesaikan Tugas5 Pratikum.

---
+++ pratikum/tugas5/latihan5d/php/admin.php
@@ -0,0 +1,96 @@
+<?php
+// Hafiszah Saidatul
+// 203040050
+// Shift Jumat 10.00 - 11.00
+?>
+
+<?php
+require 'functions.php';
+
+$lampu = query("SELECT * FROM lampu");
+?>
+<!DOCTYPE html>
+<html lang>
+
+<head>
+    <title>Admin Home Design Lights</title>
+    <link rel="stylesheet" type="text/css" href="../assets/Semantic-UI-CSS-master/Semantic-UI-CSS-master/semantic.min.css">
+    <script type="text/javascript" src="../assets/Semantic-UI-CSS-master/Semantic-UI-CSS-master/semantic.min.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+    <script src="../assets/Semantic-UI-CSS-master/Semantic-UI-CSS-master/semantic.min.js"></script>
+</head>
+
+
+<body>
+    <div class="ui sigment small stackable menu" id="small-menu" style="padding: 0 !important; margin: 0 !important;">
+        <div class="ui container">
+            <div class="left menu">
+                <div class="item"><i class="microchip icon icon huge"></i>
+                    <div class="item">
+                        <div class="ui action input">
+                            <input type="text" placeholder="Search....">
+                            <select class="ui compact selection dropdown">
+                                <option value="article">Category</option>
+                                <option value="Product">Lamps</option>
+                                <option value="all">Nama</option>
+                                <option value="all">Designer</option>
+                                <option value="all">Variant</option>
+                                <option value="all">Harga</option>
+
+                            </select>
+                            <button class="ui button olive">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="add" style="margin: 20px !important">
+        <div class="ui container">
+            <div class="ui olive basic button">
+                <a href="./tambah.php">Tambah Data Lamps</a>
+            </div>
+        </div>
+        <div class="table">
+            <div class="ui container ">
+                <table class="ui selectable celled table ui inverted olive table" style="margin: 10px !important">
+                    <tr>
+                        <th>NO.</th>
+                        <th>opsi</th>
+                        <th>Gambar</th>
+                        <th>Nama</th>
+                        <th>Designer</th>
+                        <th>Harga</th>
+                        <th>Variant</th>
+                        <th>Quantity</th>
+                    </tr>
+                    </thead>
+                    </tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($lampu as $lamps) : ?>
+                        <tr>
+                            <td><?= $i; ?></td>
+                            <td>
+                                <div class="ui buttons">
+                                    <button class="ui black button"><a href="ubah.php?id=<?= $lamps['id'] ?>">Ubah</button>
+                                    <button class="ui grey button"><a href="hapus.php?id=<?= $lamps['id'] ?>" onclick="return confirm('Hapus Data??')" style="text-decoration: none; color: white;">Hapus</a></button>
+                                </div>
+                            </td>
+
+                            <td><img src="../assets/img/<?= $lamps["img"]; ?>"></td>
+                            <td><b><?= $lamps["nama"]; ?></b></td>
+                            <td><b><?= $lamps["designer"]; ?></b></td>
+                            <td><b><?= $lamps["harga"]; ?></b></td>
+                            <td><b><?= $lamps["variant"]; ?></b></td>
+                            <td><b><?= $lamps["quantity"]; ?></b></td>
+
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach ?>
+</body>
+</div>
+</div>
+
+</html>
---
+++ pratikum/tugas5/latihan5e/php/functions.php
@@ -0,0 +1,80 @@
+<?php
+// Hafiszah Saidatul
+// 203040050
+// Shift Jumat 10.00 - 11.00
+?>
+<?php
+
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn, "pw_tubes_203040050");
+
+    return $conn;
+}
+
+function query($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+//fungsi untuk menambahkan  data didalam database
+function tambah($data)
+{
+    $conn = koneksi();
+
+    $img = htmlspecialchars($data['img']);
+    $nama = htmlspecialchars($data['nama']);
+    $designer = htmlspecialchars($data['designer']);
+    $harga = htmlspecialchars($data['harga']);
+    $variant = htmlspecialchars($data['variant']);
+    $quantity = htmlspecialchars($data['quantity']);
+
+    $query = "INSERT INTO `lampu`(`id`, `img`, `nama`, `designer`, `harga`, `variant`, `quantity`) VALUES ('', '$img', '$nama', '$designer', '$harga', '$variant', '$quantity')";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+function hapus($id)
+{
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM `lampu` WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+function ubah($data)
+{
+    $conn = koneksi();
+
+    $id = $data['id'];
+    $img = htmlspecialchars($data['img']);
+    $nama = htmlspecialchars($data['nama']);
+    $designer = htmlspecialchars($data['designer']);
+    $harga = htmlspecialchars($data['harga']);
+    $variant = htmlspecialchars($data['variant']);
+    $quantity = htmlspecialchars($data['quantity']);
+
+    $query = "UPDATE lampu
+     SET 
+     img = '$img',
+     nama = '$nama',
+     designer = '$designer',
+     harga = '$harga',
+     variant = '$variant',
+     quantity = '$quantity'
+     WHERE id = $id
+     ";
+
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+?>
